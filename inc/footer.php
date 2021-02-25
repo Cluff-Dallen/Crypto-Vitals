0aa8eae4a160b1f1885e3050f0e29f42eeb9b3b9
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-02-24 21:53:43 -0600

    updates

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <html>
-    <link rel="stylesheet" href="styles/footer.css">
+    <link rel="stylesheet" href="../styles/footer.css">
     <div id="footer">
         <hr>
         <p>Dallen R. Cluff - 2021 - <?php echo $pageTitle?> page</p>
