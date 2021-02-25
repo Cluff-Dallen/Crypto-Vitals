43d92c4ffdf59d7d1f41dfd27443ea2c0517498d
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-02-24 21:32:10 -0600

    updates

--- cryptocurrencies.php
+++ cryptocurrencies.php
@@ -8,15 +8,14 @@
   </header>
 
   <body>
-    <h3>Cryptocurrencies</h3>
-    <p>Hello World....</p>
+    <?php $pageTitle = "Home"; ?>
+    <h3><?php echo $pageTitle?></h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequuntur corrupti laudantium doloribus laborum vero accusantium rerum deserunt voluptatem dolorem, tempore temporibus magnam commodi dolore explicabo tempora eius, facilis sit.</p>
   </body>
 
   <footer>
    <?php 
-    $pageTitle = "Home"; 
     include ('inc/footer.php'); 
    ?>
   </footer>
-
 </html>
--- inc/header.php
+++ inc/header.php
@@ -3,12 +3,12 @@
 <div id="nav">
       <img src="/images/logoSlim.png" alt="Crypto Vitals logo">
       <li><a href="cryptocurrencies.php">Cryptocurrencies</a></li>
-      <li><a href="exchanges.html">Exchanges</a></li>
-      <li><a href="favorites.html">Favorites</a></li>
-      <li><a href="portfolio.html">Portfolio</a></li>
-      <li><a href="news.html">News</a></li>
-      <li><a href="converter.html">Converter</a></li>
-      <li><a href="about.html">About</a></li>
+      <li><a href="exchanges.php">Exchanges</a></li>
+      <li><a href="favorites.php">Favorites</a></li>
+      <li><a href="portfolio.php">Portfolio</a></li>
+      <li><a href="news.php">News</a></li>
+      <li><a href="converter.php">Converter</a></li>
+      <li><a href="about.php">About</a></li>
       <li><button class="btn btn-warning shadow-none">Sign in</button></li>
     </ul>
 </div>
