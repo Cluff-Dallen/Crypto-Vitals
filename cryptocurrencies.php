e0e593da35aa93255bdec048d1b8dc8e3a65d116
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-02-24 20:18:21 -0600

    updates

--- cryptocurrencies.php
+++ cryptocurrencies.php
@@ -8,21 +8,16 @@
 
     <title>Crypto Vitals</title>
   </head>
-  <body>
-    <ul id="nav">
-      <img src="/images/logoSlim.png" alt="Crypto Vitals logo">
-      <li><a href="cryptocurrencies.html">Cryptocurrencies</a></li>
-      <li><a href="exchanges.html">Exchanges</a></li>
-      <li><a href="favorites.html">Favorites</a></li>
-      <li><a href="portfolio.html">Portfolio</a></li>
-      <li><a href="news.html">News</a></li>
-      <li><a href="converter.html">Converter</a></li>
-      <li><a href="about.html">About</a></li>
-      <li><button class='btn btn-warning shadow-none'>Sign in</button>
-      </li>
-    </ul>
-    <hr>
 
+  <header>
+  <?php include 'header.php'; ?>
+  </header>
+
+  <body>
     <h3>Cryptocurrencies</h3>
   </body>
+
+  <footer>
+  <?php include 'footer.php'; ?>
+  </footer>
 </html>
