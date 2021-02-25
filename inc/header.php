f6944c7640a1547d651e43ae60b270e9eb332fa1
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-02-24 21:52:40 -0600

    updates

--- inc/header.php
+++ inc/header.php
@@ -2,13 +2,13 @@
 <link rel="stylesheet" href="../styles/header.css">
 <div id="nav">
       <img src="/images/logoSlim.png" alt="Crypto Vitals logo">
-      <li><a href="pages/cryptocurrencies.php">Cryptocurrencies</a></li>
-      <li><a href="pages/exchanges.php">Exchanges</a></li>
-      <li><a href="pages/favorites.php">Favorites</a></li>
-      <li><a href="pages/portfolio.php">Portfolio</a></li>
-      <li><a href="pages/news.php">News</a></li>
-      <li><a href="pages/converter.php">Converter</a></li>
-      <li><a href="pages/about.php">About</a></li>
+      <li><a href="cryptocurrencies.php">Cryptocurrencies</a></li>
+      <li><a href="exchanges.php">Exchanges</a></li>
+      <li><a href="favorites.php">Favorites</a></li>
+      <li><a href="portfolio.php">Portfolio</a></li>
+      <li><a href="news.php">News</a></li>
+      <li><a href="converter.php">Converter</a></li>
+      <li><a href="about.php">About</a></li>
       <li><button class="btn btn-warning shadow-none">Sign in</button></li>
     </ul>
 </div>
