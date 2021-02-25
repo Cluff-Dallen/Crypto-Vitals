de33c3c28de367340a42f73778430169421052ab
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-02-24 20:30:46 -0600

    updates

--- cryptocurrencies.php
+++ cryptocurrencies.php
@@ -1,16 +1,10 @@
 <html>
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="/styles/home-styles.css">
-    <link rel="shortcut icon" type="image/jpg" href="/images/favicon.ico"/>
-
-    <title>Crypto Vitals</title>
+  <?php include 'inc/head.php'; ?>
   </head>
 
   <header>
-  <?php include 'inc/header.php'; ?>
+    <?php include 'inc/header.php'; ?>
   </header>
 
   <body>
@@ -18,6 +12,7 @@
   </body>
 
   <footer>
-  <?php include '/inc/footer.php'; ?>
+   <?php include 'inc/footer.php'; ?>
   </footer>
+
 </html>
