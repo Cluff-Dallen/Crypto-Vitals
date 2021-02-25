e5201d5beb64e032ea2476bc7a228c5c91eaf971
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-02-24 19:58:07 -0600

    updates

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php 
+    echo '<ul id="nav">
+      <img src="/images/logoSlim.png" alt="Crypto Vitals logo">
+      <li><a href="cryptocurrencies.html">Cryptocurrencies</a></li>
+      <li><a href="exchanges.html">Exchanges</a></li>
+      <li><a href="favorites.html">Favorites</a></li>
+      <li><a href="portfolio.html">Portfolio</a></li>
+      <li><a href="news.html">News</a></li>
+      <li><a href="converter.html">Converter</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><button class="btn btn-warning shadow-none">Sign in</button>
+      </li>
+    </ul>
+    <hr>' ?>
