c0cc8316559bc51d1cfde2ab8824f68566316686
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-02-27 00:48:25 -0600

    updates

--- inc/footer.php
+++ inc/footer.php
@@ -2,6 +2,6 @@
     <link rel="stylesheet" href="../styles/footer.css">
     <div id="footer">
         <hr>
-        <p>Senior Project | APA data pulled from <a href="www.coingecko.com">www.coingecko.com</a> | Visit us on twitter | Current page is: <?php echo $pageTitle?> page</p>
+        <p>Senior Project | APA data pulled from <a href="https://www.coingecko.com/en">https://www.coingecko.com/</a> | Visit us on twitter | Current page is: <?php echo $pageTitle?> page</p>
     </div>
 </html>
