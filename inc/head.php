74cb739ec7502f7346c141a63e09801bfddbb17f
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-02-27 01:07:38 -0600

    updates

--- inc/head.php
+++ inc/head.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="shortcut icon" type="image/jpg" href="images/favicon.ico"/>
-   
+    <link rel="stylesheet" href="../styles/body.css">
+    <link rel="stylesheet" href="../styles/footer.css">
     <title>Crypto Vitals</title>
 </html>
