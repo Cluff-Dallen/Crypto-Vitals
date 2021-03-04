264d31e7cf367647116a55ede620f9793ccb4b8c
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-03-04 17:52:04 -0600

    updates

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -1,4 +1,6 @@
-var bitcoinRequestURL = 'https://www.cryptonator.com/api/full/btc-usd';
+//var bitcoinRequestURL = 'https://www.cryptonator.com/api/full/btc-usd';
+
+var bitcoinRequestURL = 'https://api.coingecko.com/api/v3/simple/price?ids=Bitcoin&vs_currencies=USD&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true';
 var requestBitcoin = new XMLHttpRequest();
 
 requestBitcoin.open('GET', bitcoinRequestURL);
@@ -6,9 +8,14 @@ requestBitcoin.send();
 
 requestBitcoin.onload = function() {
 
-    var bitcoin = JSON.parse(requestBitcoin.responseText);
+   /*var bitcoin = JSON.parse(requestBitcoin.responseText);
     document.getElementById('bitcoinTicker').innerHTML = bitcoin.ticker.base;
     document.getElementById('bitcoinPrice').innerHTML = bitcoin.ticker.price;
     document.getElementById('bitcoinVolume').innerHTML = bitcoin.ticker.volume;
-    document.getElementById('bitcoinChange').innerHTML = bitcoin.ticker.change;
+    document.getElementById('bitcoinChange').innerHTML = bitcoin.ticker.change;*/
+
+    var bitcoin = JSON.parse(requestBitcoin.responseText);
+    document.getElementById('bitcoinPrice').innerHTML = bitcoin.usd;
+    document.getElementById('bitcoinVolume').innerHTML = bitcoin.usd_24h_vol;
+    document.getElementById('bitcoinChange').innerHTML = bitcoin.usd_24h_change;
 }
