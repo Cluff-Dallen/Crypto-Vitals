95b69eeac5f244c525c1e3adf79adc3072810e5f
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-03-04 18:38:47 -0600

    updated

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -1,6 +1,4 @@
 var bitcoinRequestURL = 'https://www.cryptonator.com/api/full/btc-usd';
-
-//var bitcoinRequestURL = 'https://api.coingecko.com/api/v3/coins/bitcoin?tickers=true&market_data=true';
 var requestBitcoin = new XMLHttpRequest();
 
 requestBitcoin.open('GET', bitcoinRequestURL);
@@ -8,14 +6,9 @@ requestBitcoin.send();
 
 requestBitcoin.onload = function() {
 
-   var bitcoin = JSON.parse(requestBitcoin.responseText);
+    var bitcoin = JSON.parse(requestBitcoin.responseText);
     document.getElementById('bitcoinTicker').innerHTML = bitcoin.ticker.base;
     document.getElementById('bitcoinPrice').innerHTML = bitcoin.ticker.price;
     document.getElementById('bitcoinVolume').innerHTML = bitcoin.ticker.volume;
     document.getElementById('bitcoinChange').innerHTML = bitcoin.ticker.change;
-
-    /*var bitcoin = JSON.parse(requestBitcoin.responseText);
-    document.getElementById('bitcoinPrice').innerHTML = bitcoin.id;
-    document.getElementById('bitcoinVolume').innerHTML = bitcoin.name;
-    document.getElementById('bitcoinChange').innerHTML = bitcoin.bitcoin.symbol;*/
 }
