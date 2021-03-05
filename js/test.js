7bfa7d71ec656898921a4ac0c8a41cfe3b4bea71
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-03-05 04:41:30 -0600

    updated

--- js/test.js
+++ js/test.js
@@ -1,21 +1,32 @@
+
+//Where is the data coming from?
 var coingeckoRequestURL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=7d';
 var myRequest = new XMLHttpRequest();
 
+//How is the data coming? 
 myRequest.open('GET',  coingeckoRequestURL);
+
+//Research..
 myRequest.send();
 
+
+//More Research
 myRequest.onload = function() {
 
+    /* Create a variable called currency, and store the entire JSON text inside it.*/
     var currency = JSON.parse(myRequest.responseText);
 
+    //Create table object, link to HTML by id. 
     var table = document.getElementById("myTable");
 
-    var x = 0;
-
+    //For each item in the currency array, create a row, add data for each cell.
       for (i in currency) {
+
+        //Create new row
           console.log(currency[i].id, currency[i].symbol);
           var row = table.insertRow(table.rows.length);
          
+          //Create cells and add data inside
           var currency_rank = row.insertCell(0);
           currency_rank.innerHTML = currency[i].market_cap_rank;
 
@@ -36,7 +47,5 @@ myRequest.onload = function() {
 
           var currency_marketcap = row.insertCell(6);
           currency_marketcap.innerHTML = currency[i].market_cap;
-
-          x += 1;
       }
 }
