622b1c009d75b273e0df7eb40152f7030086c1aa
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-03-04 19:36:21 -0600

    updated

--- js/test.js
+++ js/test.js
@@ -1,4 +1,4 @@
-var coingeckoRequestURL = 'https://api.coingecko.com/api/v3/simple/price?ids=Ethereum&vs_currencies=USD&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true';
+var coingeckoRequestURL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=7d';
 var myRequest = new XMLHttpRequest();
 
 myRequest.open('GET',  coingeckoRequestURL);
