bd5e012b6bdf1cce618cd7f9b293351ce64d0790
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-03-11 10:28:11 -0600

    updates

---
+++ pages/forgot-pass.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+  <?php include '../inc/head.php'; ?>
+  </head>
+
+  <header>
+    <?php include '../inc/header.php'; ?>
+  </header>
+
+  <body>
+    <?php $pageTitle = "Forgot Password"; ?>
+    <h3><?php echo $pageTitle?></h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequuntur corrupti laudantium doloribus laborum vero accusantium rerum deserunt voluptatem dolorem, tempore temporibus magnam commodi dolore explicabo tempora eius, facilis sit.</p>
+  </body>
+
+  <footer>
+   <?php 
+    include ('../inc/footer.php'); 
+   ?>
+  </footer>
+</html>
