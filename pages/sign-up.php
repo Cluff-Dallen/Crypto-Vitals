ad15e9dbf31cac264441a581bf0f2ce774a95722
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-03-11 10:09:45 -0600

    updates

---
+++ pages/sign-up.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+  <?php include '../inc/head.php'; ?>
+  </head>
+
+  <header>
+    <?php include '../inc/header.php'; ?>
+  </header>
+
+  <body>
+    <div id="sign-up-form">
+    <h1>Sign-up</h1>
+    <form action="">
+    <input type="text" name="firstname" placeholder="Enter your first name">
+    <input type="text" name="lastname" placeholder="Enter your last name">
+    <input type="email" name="email" placeholder="Enter email address">
+    <input type="passwordOne" name="passwordOne" placeholder="Enter password">
+    <input type="passwordTwo" name="passwordtwo" placeholder="Reenter password">
+    <input type="submit" name="signupSubmit">
+    </form>
+    <p><a href="#">Register</a> | <a href="#">Forgot Password</a></p>
+    </div>
+  </body>
+
+  <footer>
+   <?php 
+    include ('../inc/footer.php'); 
+   ?>
+  </footer>
+</html>
