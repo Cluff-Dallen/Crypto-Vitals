ee0f3e631982a4519435960fa3aa496abf627b5d
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-03-11 13:04:11 -0600

    updates

--- pages/sign-up.php
+++ pages/sign-up.php
@@ -13,9 +13,9 @@
     <h1>Sign-up</h1>
     <form action="">
     <div class="inputz"><span class="material-icons">perm_identity</span><input type="text" name="username" placeholder="Username"></div>
-    <div class="inputz"><span class="material-icons">email</span><input type="email" name="email" placeholder="Email Address"></div>
-    <div class="inputz"><span class="material-icons">lock</span><input type="password" name="password" placeholder="Password"></div>
-    <div class="inputz"><span class="material-icons">lock</span><input type="passwordConfirm" name="passwordConfirm" placeholder="Confirm password"></div>
+    <div class="inputz"><span class="material-icons">mail_outline</span><input type="email" name="email" placeholder="Email Address"></div>
+    <div class="inputz"><span class="material-icons">lock_outline</span><input type="password" name="password" placeholder="Password"></div>
+    <div class="inputz"><span class="material-icons">lock_outline</span><input type="passwordConfirm" name="passwordConfirm" placeholder="Confirm password"></div>
     <div class="inputz"><input type="submit" name="signupSubmit"></div>
     </form>
     <p><a href="sign-in.php">Already have an account</a> | <a href="forgot-pass.php">Forgot Password</a></p>
