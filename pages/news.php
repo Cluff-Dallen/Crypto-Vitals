d47515401486d16cfa7f715cab29f8c68fd6b003
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-03-24 00:51:12 -0500

    updates

--- pages/news.php
+++ pages/news.php
@@ -10,8 +10,7 @@
   <body>
     <?php $pageTitle = "News"; ?>
     <h3><?php echo $pageTitle?></h3>
-    <a href="https://twitter.com/intent/tweet?button_hashtag=Warzone&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #LoveTwitter</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequuntur corrupti laudantium doloribus laborum vero accusantium rerum deserunt voluptatem dolorem, tempore temporibus magnam commodi dolore explicabo tempora eius, facilis sit.</p>
-  </body>
+    <a class="twitter-timeline" href="https://twitter.com/Dallen_C?ref_src=twsrc%5Etfw">Tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>  </body>
 
   <footer>
    <?php 
