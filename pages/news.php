251c81d60d68f599ebd6f3e6bf77808687739c96
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-03-24 00:55:25 -0500

    updates

--- pages/news.php
+++ pages/news.php
@@ -10,7 +10,8 @@
   <body>
     <?php $pageTitle = "News"; ?>
     <h3><?php echo $pageTitle?></h3>
-    <a class="twitter-timeline" href="https://twitter.com/Dallen_C">Tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>  </body>
+    <a class="twitter-timeline" data-width="500" data-height="1000" data-theme="dark" href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw">Tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </body>
 
   <footer>
    <?php 
