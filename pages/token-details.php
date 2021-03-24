2700998df5931b73ef10634afafb575875ae48e3
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-03-24 09:14:49 -0500

    updates

--- pages/token-details.php
+++ pages/token-details.php
@@ -9,8 +9,28 @@
 </header>
 
 <body>
-<?php $name = $_REQUEST['id'];?>
-  <h3><?php echo $name ?></h3>
+<?php $token =$_GET['id'];?>
+<script type="text/javascript">
+    var asset = <?php Print($token); ?>;
+
+var coingeckoRequestURL = 'https://api.coingecko.com/api/v3/simple/price?ids=Ethereum&vs_currencies=USD&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true';
+var myRequest = new XMLHttpRequest();
+
+myRequest.open('GET', coingeckoRequestURL);
+myRequest.send();
+
+myRequest.onload = function () {
+
+    var currency = JSON.parse(myRequest.responseText);
+    document.getElementById('price2').innerHTML = currency.ethereum.usd;
+    document.getElementById('vol2').innerHTML = currency.ethereum.usd_24h_vol;
+}
+
+</script>
+  <h3><?php echo $token?></h3>
+  <span id="price2">price</span>
+  <span id="vol2">volume</span>
+
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequuntur corrupti laudantium doloribus laborum vero accusantium rerum deserunt voluptatem dolorem, tempore temporibus magnam commodi dolore explicabo tempora eius, facilis sit.</p>
 </body>
 
