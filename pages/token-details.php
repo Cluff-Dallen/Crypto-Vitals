fb2942e571068c6d75eef4483a2e51416ee8eb70
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-03-24 09:23:21 -0500

    updates

--- pages/token-details.php
+++ pages/token-details.php
@@ -13,7 +13,7 @@
 <script type="text/javascript">
     var asset = <?php Print($token); ?>;
 
-var coingeckoRequestURL = 'https://api.coingecko.com/api/v3/simple/price?ids=Ethereum&vs_currencies=USD&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true';
+var coingeckoRequestURL = "https://api.coingecko.com/api/v3/simple/price?ids=" + asset + "&vs_currencies=USD&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true";
 var myRequest = new XMLHttpRequest();
 
 myRequest.open('GET', coingeckoRequestURL);
@@ -22,14 +22,20 @@ myRequest.send();
 myRequest.onload = function () {
 
     var currency = JSON.parse(myRequest.responseText);
-    document.getElementById('price2').innerHTML = currency.ethereum.usd;
-    document.getElementById('vol2').innerHTML = currency.ethereum.usd_24h_vol;
+
+    var x = asset;
+    var cprice = currency[x].usd;
+    var cVolume = currency[x].usd_24h_vol;
+
+
+    document.getElementById('price').innerHTML = cPrice;
+    document.getElementById('volume').innerHTML = cVolume;
 }
 
 </script>
   <h3><?php echo $token?></h3>
-  <span id="price2">price</span>
-  <span id="vol2">volume</span>
+  <span id="price">price</span>
+  <span id="volume">volume</span>
 
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequuntur corrupti laudantium doloribus laborum vero accusantium rerum deserunt voluptatem dolorem, tempore temporibus magnam commodi dolore explicabo tempora eius, facilis sit.</p>
 </body>
