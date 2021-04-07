987464524bccd643dfbe6c423aaf75aa5e4f554b
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-04-07 13:11:25 -0500

    updates

--- pages/sign-in-result.php
+++ pages/sign-in-result.php
@@ -69,9 +69,6 @@ while ($row = pg_fetch_row($result)) {
   array_push($favorites, $row[1]);
 }
 $_SESSION["favoriteList"] = implode( ", ", $favorites ); 
-
-echo $_SESSION["favoriteList"];
-
 ?>
 
 <body>
