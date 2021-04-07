4c1f059ddcf1791dfdd014b5676aa159e3a390b5
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-04-07 04:04:52 -0500

    updates

--- pages/sign-in-result.php
+++ pages/sign-in-result.php
@@ -57,14 +57,13 @@ if (!$conn) {
   echo "An error occurred.\n";
   exit;
 }
-$result = pg_query($conn, "SELECT * FROM users");
+$result = pg_query($conn, "SELECT * FROM favorites WHERE emailOfThisFavorite = 'cluffrdallen@gmail.com'");
 if (!$result) {
   echo "An error occurred.\n";
   exit;
 }
-
 while ($row = pg_fetch_row($result)) {
-  echo "Author: $row[0]  E-mail: $row[1]";
+  echo "Currencies: $row[1]";
   echo "<br />\n";
 }
 
