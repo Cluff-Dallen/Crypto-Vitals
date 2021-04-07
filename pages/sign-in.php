fe96f3848ff696ef64e9bdabdbd119e74a36c843
Cluff-Dallen <cluffrdallen@protonmail.com>
2021-04-07 03:01:43 -0500

    updates

--- pages/sign-in.php
+++ pages/sign-in.php
@@ -1,9 +1,7 @@
-<?php 
-session_start();
+<?php
+  session_start();
+  require_once "../db/dbConnect.php";
 ?>
-<?php require_once "../db/dbConnect.php";
-?>
-
 
 <html>
 <head>
@@ -16,16 +14,11 @@ session_start();
 </header>
 
 <body>
-<?php 
-
-echo "Welcome, " . $_SESSION["currentUser"];
-
-?>
   <div id="sign-in-form">
     <h1>Sign-in</h1>
     <form action="sign-in-result.php" method="POST">
       <div class="signinInput"><span class="material-icons">mail_outline</span><input type="email" name="email" placeholder="Email address" required></div>
-      <div class="signinInput"><span class="material-icons">lock_outline</span><input type="password" name="password" placeholder="Password"required></div>
+      <div class="signinInput"><span class="material-icons">lock_outline</span><input type="password" name="password" placeholder="Password" required></div>
       <div class="signinInput"><input type="submit" id="signinSubmit" name="loginSubmit"></div>
     </form>
     <p><a href="sign-up.php">Register</a> | <a href="forgot-pass.php">Forgot Password</a></p>
